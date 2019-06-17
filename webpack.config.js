16c3434dc5338b8bd4cb19afec72119a8a4bd003
shaho <toofani@gmail.com>
2019-06-17 18:17:38 +0430

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./src/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          "style-loader", // creates style nodes from JS strings
+          "css-loader", // translates CSS into CommonJS
+          "sass-loader" // compiles Sass to CSS, using Node Sass by default
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"]
+  },
+  output: {
+    path: __dirname + "/public/scripts",
+    publicPath: "/scripts",
+    filename: "bundle.js"
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+  devServer: {
+    contentBase: "./public",
+    hot: true
+  },
+  devtool: "source-map"
+};
